d07d34de2ca4ad351f81a4658d855a9af23ef248
picklecai <52421240@qq.com>
2016-11-18 17:31:46 +0800

    update

--- mission9-1115/book.js
+++ mission9-1115/book.js
@@ -1,6 +1,13 @@
 //No.1
-do
+
+function favbook(list_length)
 {
-	var book = {name: prompt("请输入书名："), author: prompt("请输入该书的作者："), score: prompt("请输入该书的豆瓣评分：")};
+	var Book = Object();
+	for (i=0, i<list_length, i++)
+	{
+		var book = new Book;
+		book = {name: prompt("请输入书名："), author: prompt("请输入该书的作者："), score: prompt("请输入该书的豆瓣评分：")};
+	}
 }
-while(book.name != "-")
+
+favbook(2);
