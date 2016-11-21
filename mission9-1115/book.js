2d158ba6f8d6c5fc3eae424c26b9880485a44e0b
picklecai <pickle.ahcai@163.com>
2016-11-22 07:31:03 +0800

    update
    完成了第一部分

--- mission9-1115/book.js
+++ mission9-1115/book.js
@@ -1,13 +1,29 @@
 //No.1
 
-function favbook(list_length)
+//定义对象和数组
+var book_list = new Array();
+function Book(name, author, score)
 {
-	var Book = Object();
-	for (i=0, i<list_length, i++)
-	{
-		var book = new Book;
-		book = {name: prompt("请输入书名："), author: prompt("请输入该书的作者："), score: prompt("请输入该书的豆瓣评分：")};
-	}
+	this.Name = name;
+	this.Author = author;
+	this.Score = score;
 }
 
-favbook(2);
+//定义均值计算函数
+function average_score(list_num)
+{
+	var sum_score = 0;
+	for(var i=0; i<list_num; i++)  //这里是分号，不是逗号
+	{
+		book_list[i] = new Book(prompt("name"), prompt("author"), prompt("score"));
+		sum_score += parseFloat(book_list[i].Score);
+	}
+
+	alert(sum_score/list_num);
+	//console.log("%d", book_list[0].Score);
+}
+
+//调用均值计算函数
+average_score(3);
+
+//No.2
