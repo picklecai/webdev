5c453e8d756f08ac83ab28e7162c06a8126beb39
picklecai <pickle.ahcai@163.com>
2016-12-25 07:43:01 +0800

    第二个函数调用要写括号

--- mission12-1214/gallery.js
+++ mission12-1214/gallery.js
@@ -1,6 +1,17 @@
 function showPic(whichpic)
 {
 	var source = whichpic.getAttribute("href");
+	var text = whichpic.getAttribute("title");
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
+	//var para = document.createElement("p");
+	//para.appendChild(text);
+
+	function countBodyChildren()
+	{
+		var body_element = document.getElementsByTagName("body")[0];
+		alert(body_element.childNodes.length);
+	}
+
+	window.onload = countBodyChildren();  //书上这里不写括号，即不写参数。在这个浏览器下不执行。只有加了括号才执行这个函数。
 }
