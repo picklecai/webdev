84358fac0a338abc6b39062fdef32b4dbe110822
picklecai <pickle.ahcai@163.com>
2017-02-16 02:22:23 +0800

    减少参与循环的内容

--- mission16-0117/screensaver.js
+++ mission16-0117/screensaver.js
@@ -1,16 +1,11 @@
-window.onload = function move()
-{
-	//setInterval("draw()", 500);
-	setTimeout("draw()", 500);
-}
+var canvas = document.getElementById("screensaver");
+//var width = canvas.style.width;
+//var height = canvas.style.height;
+width = 1300;
+height = 550;
 
 function draw()
 {
-	var canvas = document.getElementById("screensaver");
-	//var width = canvas.style.width;
-	//var height = canvas.style.height;
-	width = 1300;
-	height = 550;
 	if(canvas.getContext)
 	{
 		var context = canvas.getContext("2d");
@@ -19,25 +14,19 @@ function draw()
 		var w = 50;	
 		//var w = Math.floor(Math.random()*40);
 		var x = Math.floor(Math.random() * canvas.width);
-		if (x<w) 
-			{
-				x=w;
-			}
-		else if((width-x)<w) 
-			{
-				x=width-w;
-			}
+		if (x<w) x=w;
+		else if((width-x)<w) x=width-w;
 		var y = Math.floor(Math.random() * canvas.height);
-		if (y<w) 
-			{
-				y=w;
-			}
-		else if((height-y)<w) 
-			{
-				y=height-w;
-			}		
+		if (y<w) y=w;
+		else if((height-y)<w) y=height-w;	
 		context.fillStyle = "lightblue";
 		context.fillRect(x, y, w, w);
 		setTimeout("draw()", 500);
 	}
 }			
+
+window.onload = function move()
+{
+	//setInterval("draw()", 500);
+	setTimeout("draw()", 500);
+}
