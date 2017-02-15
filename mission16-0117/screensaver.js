fbee787f91e3861a9de458179bee9c1fccad71dc
picklecai <52421240@qq.com>
2017-02-15 08:16:48 +0800

    让正方形不再掉落到画布之外

--- mission16-0117/screensaver.js
+++ mission16-0117/screensaver.js
@@ -6,15 +6,33 @@ window.onload = function move()
 function draw()
 {
 	var canvas = document.getElementById("screensaver");
+	width = 1300;
+	height = 550;
 	if(canvas.getContext)
 	{
 		var context = canvas.getContext("2d");
 		context.save();
-		context.clearRect(0,0,1300, 550);	
+		context.clearRect(0,0,width, height);	
 		var w = 50;	
 		//var w = Math.floor(Math.random()*40);
 		var x = Math.floor(Math.random() * canvas.width);
+		if (x<w) 
+			{
+				x=w;
+			}
+		else if((width-x)<w) 
+			{
+				x=width-w;
+			}
 		var y = Math.floor(Math.random() * canvas.height);
+		if (y<w) 
+			{
+				y=w;
+			}
+		else if((height-y)<w) 
+			{
+				y=height-w;
+			}		
 		context.fillStyle = "lightblue";
 		context.fillRect(x, y, w, w);
 	}
