edca3eb84993d0a71cdf4e95d13e1148dabb7793
picklecai <pickle.ahcai@163.com>
2017-02-26 17:32:06 +0800

    格子画好了

--- mission17-0219/lifegame.js
+++ mission17-0219/lifegame.js
@@ -1,25 +1,67 @@
-var canvas = document.getElementById("lifegame");
-var width = canvas.offsetWidth;
-var height = canvas.offsetHeight;
-var step = 10px;
-
-//定义单元格对象
-var cells = {};
-var wn = width/step;
-var hn = height/step;
-var n = wn * hn;
-for(i=0, i++, i<n)
+window.onload = function lifegame()
 {
-	
+	var context = canvas.getContext("2d");
+	drawLine(context);	
+	setInterval("updateCanvas()", 500);
+}
+
+var canvas = document.getElementById("lifegame");
+var step = 50;
+var wn = canvas.offsetWidth/step, hn = canvas.offsetHeight/step;
+
+function drawLine(context)
+{
+	context.beginPath();
+	context.lineWidth = 1;
+	for(i=0; i<wn; i++)
+	{
+		context.moveTo(i*step,0);
+		context.lineTo(i*step, canvas.offsetHeight);
+	}	
+	for(j=0; j<hn; j++)
+	{
+		context.moveTo(0, j*step);
+		context.lineTo(canvas.offsetWidth, j*step);
+	}
+	context.closePath();
+	context.stroke();
+}
+
+function updateCanvas(context, cellMatrix)
+{
+	updateCellMatrix(cellMatrix);
+	drawCanvas(context);
+}
+
+function updateCellMatrix(cellMatrix)
+{
+	for(i=0; i<wn; i++)
+		for(j=0; j<hn; j++)
+		{
+			console.log("cell i,j is drawed");
+		}
+}
+
+function drawCanvas(context)
+{
+	console.log("Draw Canvas");
 }
 
 
 /*
+
+//定义单元格对象
+var cells = {};
+
+var n = wn * hn;
+
+
+
 function draw()
 {
 	if(canvas.getContext)
 	{
-		var context = canvas.getContext("2d");
+
 		//context.save(); //删掉一点影响都没有。
 		context.clearRect(0,0,width, height);	
 		var w = 50;	
@@ -41,3 +83,4 @@ window.onload = function move()
 	//setInterval("draw()", 500);
 	setTimeout("draw()", 500);
 }
+*/
