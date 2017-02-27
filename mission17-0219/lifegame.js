78f799e8636df963d918c1a9ec3eed27299bd5a6
picklecai <pickle.ahcai@163.com>
2017-02-27 19:42:55 +0800

    又回到随机初始化状态，但彩色没有出现

--- mission17-0219/lifegame.js
+++ mission17-0219/lifegame.js
@@ -4,24 +4,23 @@ window.onload = function lifegame()
 	var rn = 20;	
 	drawLine(context);
 	drawRandom(context, rn);
-	updateCanvas();
-	//setInterval("updateCanvas()", 500);
+	//updateCanvas();
+	setInterval("updateCanvas()", 500);
 }
 
 var canvas = document.getElementById("lifegame");
 var step = 50;
 var wn = canvas.offsetWidth/step, hn = canvas.offsetHeight/step, n = wn * hn;
 var cells = [];
-console.log(wn, hn);
+//console.log(wn, hn);//浮点数
 for(i=0; i<wn; i++)
-	if(!cells[i])cells[i] = [];
-	for(j=0; j<hn; j++)
-	{		
-		
-		//cells[i] = {"x":i*step, "flag":0, "ctxCells":0};
-		//cells[i][j] = {"x":i*step, "y":j*step, "flag":0, "ctxCells":0};
-	}	
-
+	{
+		if(!cells[i])cells[i] = [];
+		for(j=0; j<hn; j++)
+		{
+			cells[i][j] = {"x":i*step, "y":j*step, "flag":0, "ctxCells":0};
+		}
+	}
 function drawLine(context)
 {
 	context.beginPath();
@@ -46,31 +45,28 @@ function drawRandom(context, rn)
 	{
 		var x = step * (Math.floor(Math.random() * wn));
 		var y = step * (Math.floor(Math.random() * hn));
-		fillCell(context, x, y);		
+		fillCell(context, x/step, y/step);
 	}
 }
 
 function updateCanvas(context, cell)
 {
-	updateCellMatrix(cell);
-	context.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);	
-	drawCanvas(context);
-}
-
-function updateCellMatrix(cell)
-{
 	for(i=0; i<wn; i++)
+	{
 		for(j=0; j<hn; j++)
 		{
-			console.log("cell i,j is drawed");
+			context.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);	
+			drawCanvas(context, i, j);			
 		}
+	}	
 }
 
 function fillCell(context, i, j)
 {
 	x = i * step;
 	y = j *step;
-	context.fillRect(x, y, step, step);	
+	context.fillRect(x, y, step, step);
+	cells[i][j].flag = 1;
 }
 
 function discolorCell(context, i, j)
@@ -78,29 +74,31 @@ function discolorCell(context, i, j)
 	x = i * step;
 	y = j *step;	
 	context.fillStyle = "white";
-	context.fillRect(x, y, step, step);		
+	context.fillRect(x, y, step, step);	
+	cells[i][j].flag = 0;	
+}
+
+function getCtxCells(i, j)
+{
+	var ctxCells = cells[i-1][j-1].flag + cells[i][j-1].flag + cells[i+1][j-1].flag  + cells[i-1][j].flag + cells[i+1][j].flag + cells[i-1][j+1].flag + cells[i][j+1].flag + cells[i+1][j+1].flag;   
+	return ctxCells;
 }
 
 function drawCanvas(context, i, j)
 {
 	x = i * step;
-	y = j *step;		
-	if (cell[i][j].ctxCells == 3)
+	y = j *step;
+	cells[i][j].ctxCells = getCtxCells(i, j);
+	if (cells[i][j].ctxCells == 3)
 	{
-		fillCell(context, x, y);
+		fillCell(context, i, j);
 	}
-	else if (cell[i][j].ctxCells <2 || cell[i][j].ctxCells >3)
+	else if (cells[i][j].ctxCells <2 || cells[i][j].ctxCells >3)
 	{
-		discolorCell(context, x, y);
+		discolorCell(context, i, j);
 	}
-	else if (cell[i][j].ctxCells ==2)
+	else if (cells[i][j].ctxCells ==2)
 	{
 		return;
 	}
-}
-
-function getCtxCells(i, j)
-{
-	var ctxCells = cell[i-1][j-1].flag + cell[i][j-1].flag + cell[i+1][j-1].flag  + cell[i-1][j].flag + cell[i+1][j].flag + cell[i-1][j+1].flag + cell[i][j+1].flag + cell[i+1][j+1].flag;   
-	return ctxCells;
 }
